959b94a97ab788a39653b01a5968b592885543f6
Joseph Manning <jgmanning3@yahoo.com>
2021-06-02 10:01:58 -0700

    Rendered questions and answers to page.

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ var startBtn = document.querySelector("#startBtn")
 var startQuiz = document.querySelector("#start-quiz")
 var quiz = document.querySelector("#quiz")
 var timer = document.querySelector("#timer")
+var questionText = document.querySelector("#question-text")
+var optionA = document.querySelector("#optionA")
+var optionB = document.querySelector("#optionB")
+var optionC = document.querySelector("#optionC")
+var optionD = document.querySelector("#optionD")
+var questionIndex = 0
+var answerIndex = 0
+// var runningQuestion = 0
+// var lastQuestion = questions.length - 1;
 
 // Shows/hides opening message/button and first question
 startBtn.addEventListener("click", function (e) {
     startQuiz.classList.add("hidden");
     quiz.classList.remove("hidden");
+    renderQuestion();
+    // next()
+    // question.classList.remove("hidden");
+    // optionA.classList.remove("hidden");
+    // optionB.classList.remove("hidden");
+    // optionC.classList.remove("hidden");
+    // optionD.classList.remove("hidden");
 
 })
 
@@ -25,3 +41,103 @@ startBtn.addEventListener("click", function setTime() {
   
     }, 1000);
   })
+
+  // Quiz questions set as arrays
+var questions = [
+  {
+    question: "How many ears do humans have?",
+    answers: {
+      a: "5",
+      b: "3",
+      c: "2",
+      d: "4",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "How many eyes do humans have?",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "2",
+      d: "4",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "How many toes do humans have?",
+    answers: {
+      a: "2",
+      b: "13",
+      c: "6",
+      d: "10",
+    },
+    correctAnswer: "d",
+  },  // {
+  //   question: ""
+  //   optionA: ""
+  //   optionB: ""
+  //   optionC: ""
+  //   optionD: ""
+  //   answer: ""
+  // }
+  // {
+  //   question: ""
+  //   optionA: ""
+  //   optionB: ""
+  //   optionC: ""
+  //   optionD: ""
+  //   answer: ""
+  // }
+  // {
+  //   question: ""
+  //   optionA: ""
+  //   optionB: ""
+  //   optionC: ""
+  //   optionD: ""
+  //   answer: ""
+  // }
+  // {
+  //   question: ""
+  //   optionA: ""
+  //   optionB: ""
+  //   optionC: ""
+  //   optionD: ""
+  //   answer: ""
+  // }
+  // {
+  //   question: ""
+  //   optionA: ""
+  //   optionB: ""
+  //   optionC: ""
+  //   optionD: ""
+  //   answer: ""
+  // }
+  // {
+  //   question: ""
+  //   optionA: ""
+  //   optionB: ""
+  //   optionC: ""
+  //   optionD: ""
+  //   answer: ""
+  // }
+  // {
+  //   question: ""
+  //   optionA: ""
+  //   optionB: ""
+  //   optionC: ""
+  //   optionD: ""
+  //   answer: ""
+  // 
+]
+// debugger
+function renderQuestion() {
+  var question = questions[questionIndex]
+  var answers = questions[answerIndex]
+  console.log(question)
+  questionText.textContent = question.question
+  optionA.textContent = answers.answers.a
+  optionB.textContent = answers.answers.b
+  optionC.textContent = answers.answers.c
+  optionD.textContent = answers.answers.d
+}
